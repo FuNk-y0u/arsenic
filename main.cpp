67db74073f619203dc300362d8a942235657b13d
FuNk-y0u <robesckeydangol2006@gmail.com>
2025-06-22 15:54:21 +0545

    Added view matrix support

--- main.cpp
+++ main.cpp
@@ -15,8 +15,9 @@ int main()
 "layout(location=0) in vec2 position;\n"
 "uniform mat4 u_ModelMatrix;\n"
 "uniform mat4 u_PerspectiveMatrix;\n"
+"uniform mat4 u_ViewMatrix;\n"
 "void main(){\n"
-"   gl_Position = u_PerspectiveMatrix * u_ModelMatrix * vec4(position, 0.0f, 1.0f);\n"
+"   gl_Position = u_PerspectiveMatrix * u_ViewMatrix * u_ModelMatrix * vec4(position, 0.0f, 1.0f);\n"
 "}\n";
 
     const char* fragment_shader= 
@@ -37,6 +38,10 @@ int main()
     Vertices positions = { 0.0f,  0.5f, -0.5f, -0.5f, 0.5f, -0.5f};
 
     f32 offset = -1.50f;
+
+    glm::vec3 cam_pos = glm::vec3(0.0f, 0.0f, 5.0f);
+    glm::vec3 center = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
    
 
     while (running) {
@@ -77,12 +82,23 @@ int main()
         else{
             std::cout << "UNIFORM: Canoot find u_PerspectiveMatrix" << std::endl;
         }
+
+        GLint view_matrix_location = glGetUniformLocation(renderer.render_shader.id, "u_ViewMatrix");
+
+
+        if( view_matrix_location >= 0) {
+        }
+        else{
+            std::cout << "UNIFORM: Canoot find u_ViewMatrix" << std::endl;
+        }
         
         glm::mat4 perspective_matrix = glm::perspective(glm::radians(45.0f), (float)640/(float)480, 0.1f, 10.0f);
         glm::mat4 model_matrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, offset));
+        glm::mat4 view_matrix = glm::lookAt(cam_pos, center, up);
 
         glUniformMatrix4fv(model_matrix_location, 1, GL_FALSE, &model_matrix[0][0]);
         glUniformMatrix4fv(perspective_matrix_location, 1, GL_FALSE, &perspective_matrix[0][0]);
+        glUniformMatrix4fv(view_matrix_location, 1, GL_FALSE, &view_matrix[0][0]);
         
 
         renderer.end();
